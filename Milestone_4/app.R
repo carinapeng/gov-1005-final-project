24a84953ee20de2d83cf8dc1cc179f2c7db38261
Eliot Min <eliotmin@dhcp-10-250-15-160.harvard.edu>
2020-03-06 20:00:24 -0500

    Shiny app for milestone 4

--- Milestone_4/app.R
+++ Milestone_4/app.R
@@ -1,23 +1,21 @@
 library(shiny)
+library(plotly)
+library(ggthemes)
+
+data <- readRDS("for_shiny.RDS")
 
 ui <- navbarPage(
     "Blocking Project",
     tabPanel("Model",
              fluidPage(
                  titlePanel("Fake Pre-Housing Day Data"),
-                 sidebarLayout(
-                     sidebarPanel(
-                         selectInput(
-                             "plot_type",
-                             "Plot Type",
-                             c("Option A" = "a", "Option B" = "b")
-                         )),
-                     mainPanel(plotOutput("line_plot")))
-             )),
+                 mainPanel(plotlyOutput("plotName"),
+                           plotlyOutput("plotName2")
+             ))),
+    
     tabPanel("Discussion",
              titlePanel("Conclusions from Fake Data"),
-             p("Tour of the modeling choices you made and 
-              an explanation of why you made them")),
+             p("We inputted model data into the survey we made to get preliminary graphs. More detailed work will follow with actual data collection and analysis.")),
     tabPanel("About", 
              titlePanel("About"),
              h3("Project Background and Motivations"),
@@ -26,34 +24,33 @@ ui <- navbarPage(
 
 The project will attempt to replicate a truly random housing lottery to assess if Harvard’s housing appears to be random as well. The project will check variables such as legacy, race, religion, athletics, sex, freshman dorm, financial aid, international students, and blocking group size.
 
-All Sensitive questions have a “prefer not to answer” option"),
+All Sensitive questions have a “prefer not to answer” option."),
              h3("About Us"),
-             p("We are a group of nine students who sought to continue the work of GOV 1005 students from last year. 
+             p("We are a group of nine students who sought to continue the work of GOV 1005 students from last year. We are
+             Jamal Nimer, Carina Peng, Ilyas Mardin, Shojeh Liu, Eliot Min, Lucy He, Angie Shin, Austin Li, and Sam Saba.
                ")))
 
 
 server <- function(input, output) {
-    output$line_plot <- renderPlot({
-        # Generate type based on input$plot_type from ui
-        
-        ifelse(
-            input$plot_type == "a",
-            
-            # If input$plot_type is "a", plot histogram of "waiting" column 
-            # from the faithful dataframe
-            
-            x   <- faithful[, 2],
-            
-            # If input$plot_type is "b", plot histogram of "eruptions" column
-            # from the faithful dataframe
-            
-            x   <- faithful[, 1]
-        )
-        
-        # Draw the histogram with the specified number of bins
-        
-        hist(x, col = 'darkgray', border = 'white')
-    })
+  
+  output$plotName <- renderPlotly(
+    ggplotly(
+     ggplot(data, aes(x = freshman_dorm))  +
+        geom_bar() +
+      labs(x = "Freshman House",
+           y = "Number of Students")))
+    
+    output$plotName2 <- renderPlotly(
+      ggplotly(
+        ggplot(data, aes(x = what_house_were_you_placed_in)) + 
+          geom_bar() +
+          labs(x = "House Placement",
+               y = "Number of Students")
+    ))
+    
+  
+  
+  
 }
 
 shinyApp(ui, server)
